b356b398c9ef1f1384fd556a513f228ede78fb5d
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-01-12 14:42:29 +0530

    Changed app's min SDK version to Oreo. Target SDK version is Pie.

--- app/src/main/java/com/example/reesh/pointofnews/MainActivity.java
+++ app/src/main/java/com/example/reesh/pointofnews/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.reesh.pointofnews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final SearchView searchView=(SearchView)findViewById(R.id.searchView);
+
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchView.onActionViewExpanded();
+            }
+        });
     }
 }
