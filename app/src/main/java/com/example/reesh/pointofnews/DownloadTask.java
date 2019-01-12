78905da94deee0c5c34bada146e305b0ca65a5df
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-01-12 20:40:39 +0530

    Added a compatible version of Picasso.

---
+++ app/src/main/java/com/example/reesh/pointofnews/DownloadTask.java
@@ -0,0 +1,10 @@
+//package com.example.reesh.pointofnews;
+//
+//import android.os.AsyncTask;
+//
+//public class DownloadTask extends AsyncTask<Void,Void,Void> {
+//    @Override
+//    protected Void doInBackground(Void... voids) {
+////        URL
+//    }
+//}
