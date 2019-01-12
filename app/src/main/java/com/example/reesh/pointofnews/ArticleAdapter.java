a5ded89de0c9d2d8e2db72a9e29d126e99596d45
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-01-12 18:55:21 +0530

    Debugged the whole project (problems were due to incompatible migration to AndroidX).

---
+++ app/src/main/java/com/example/reesh/pointofnews/ArticleAdapter.java
@@ -0,0 +1,42 @@
+package com.example.reesh.pointofnews;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleAdapter extends ArrayAdapter {
+
+    private ArrayList objects;
+
+    public ArticleAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+        super(context, resource, objects);
+        this.objects=(ArrayList) objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View articleView=convertView;
+
+        if(articleView==null){
+            articleView=LayoutInflater.from(getContext()).inflate(
+                    R.layout.news_item,parent,false
+            );
+        }
+
+        Article article=(Article) objects.get(position);
+        TextView headlineTextView=articleView.findViewById(R.id.headlineTextView);
+        TextView previewTextView=articleView.findViewById(R.id.previewTextView);
+        //TODO set analysis icon
+
+        return articleView;
+    }
+}
